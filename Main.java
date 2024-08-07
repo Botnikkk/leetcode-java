d5321e690720bb635022148ceec840a6dbfe65f5
Nikhil Chaudhary <Nikhil.ace.chd@gmail.com>
2024-08-08 00:22:48 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,12 +1,12 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        int[] array = {1,1,2};
+        int[] array = {9};
         Solution SolutionObject = new Solution();
-        SolutionObject.removeDuplicates(array);
-        System.out.println();
-        for(int x : array){System.out.print(x+",");}
+        int[] plusOne = SolutionObject.plusOne(array);
+        for(int x : plusOne){System.out.print(x + ",");}
     }
 
 
@@ -14,9 +14,60 @@ public class Main {
 
 class Solution{
 
-    
+    /*Q5 - https://leetcode.com/problems/plus-one/description/ */
 
-    /*https://leetcode.com/problems/remove-duplicates-from-sorted-array/ */
+    public int[] plusOne(int[] digits) {
+        for(int x = digits.length - 1; x >=0 ; x-- ){
+            if(digits[x] == 9){
+                digits[x] = 0;
+            }else{
+                digits[x] = digits[x] + 1;
+                return digits;
+            }
+        }
+        int[] output = new int[digits.length + 1];
+        Arrays.fill(output, 0);
+        output[0] = 1;
+        return output;
+    }
+
+    /*Q4 - https://leetcode.com/problems/search-insert-position/ */
+
+    public int searchInsert(int[] nums, int target) {
+        for(int x = 0; x < nums.length; x++){
+            if(nums[x] == target || nums[x] > target){
+                return x;
+            }
+        }
+        return nums.length;
+    }
+
+    /*Q3 - https://leetcode.com/problems/remove-element/*/
+
+    public int removeElement(int[] nums, int val) {     
+        ArrayList<Integer> ArrayList = new ArrayList<Integer>();
+        
+        for(int x : nums){
+            if(x == val){
+                continue;
+            }else{
+                ArrayList.add(x);
+            }
+        }
+
+        for(int x = 0; x < nums.length; x++ ){
+            if(x < ArrayList.size()){
+                nums[x] = ArrayList.get(x); 
+            }else{
+                nums[x] = 0;
+            }
+        }
+
+        return ArrayList.size();
+        
+    }
+    
+    /*Q2 - https://leetcode.com/problems/remove-duplicates-from-sorted-array/ */
 
     public int removeDuplicates(int[] nums) {
         ArrayList<Integer> ArrayList = new ArrayList<Integer>();
@@ -39,7 +90,7 @@ class Solution{
         return ArrayList.size();
     }
 
-    /*https://leetcode.com/problems/two-sum/ */
+    /*Q1 - https://leetcode.com/problems/two-sum/ */
 
     public int[] twoSum(int[] nums, int target) {
         int[] output = new int[2];
